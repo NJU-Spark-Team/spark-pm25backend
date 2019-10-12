38bbcffc47ebe31ed45e2c511525c1caf28d5a52
nosolution <fudeng125@gmail.com>
2019-10-12 21:52:09 +0800

    fix bugs

--- src/main/java/com/naived/sparkpm25backend/controller/PMReceiverController.java
+++ src/main/java/com/naived/sparkpm25backend/controller/PMReceiverController.java
@@ -2,13 +2,14 @@ package com.naived.sparkpm25backend.controller;
 
 import com.naived.sparkpm25backend.websocket.PMInfoWebsocket;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PMReceiverController {
 
     @PostMapping(value = "/submit")
-    public void submitPMInfo(String info){
+    public void submitPMInfo(@RequestBody String info){
         System.out.println(info);
         PMInfoWebsocket.sendAll(info);
     }
